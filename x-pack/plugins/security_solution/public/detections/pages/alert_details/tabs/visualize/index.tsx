1887221f83d8563db7a8f0b644d52ec671476a65
Michael Olorunnisola <michael.olorunnisola@elastic.co>
2022-11-28 15:44:33 -0500

    vidsualize in progress

---
+++ x-pack/plugins/security_solution/public/detections/pages/alert_details/tabs/visualize/index.tsx
@@ -0,0 +1,135 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React, { useState } from 'react';
+import { EuiSpacer, EuiFlexGroup, EuiButtonGroup, EuiTitle } from '@elastic/eui';
+import type { TimelineEventsDetailsItem } from '@kbn/timelines-plugin/common';
+import styled from 'styled-components';
+import {
+  useSessionView,
+  useSessionViewNavigation,
+} from '../../../../../timelines/components/timeline/session_tab_content/use_session_view';
+import { TableId } from '../../../../../../common/types';
+import type { SearchHit } from '../../../../../../common/search_strategy';
+import { useTimelineDataFilters } from '../../../../../timelines/containers/use_timeline_data_filters';
+import { Resolver } from '../../../../../resolver/view';
+import { useSourcererDataView } from '../../../../../common/containers/sourcerer';
+import { JsonView } from '../../../../../common/components/event_details/json_view';
+import { EventFieldsBrowser } from '../../../../../common/components/event_details/event_fields_browser';
+import { SourcererScopeName } from '../../../../../common/store/sourcerer/model';
+
+const SessionViewWrapper = styled.div`
+  ${({ theme }) => `margin: 0 ${theme.eui.euiSizeM};`}
+`;
+
+export const AlertDetailsVisualizeTab = React.memo(
+  ({
+    data,
+    searchHit,
+    id,
+  }: {
+    data: TimelineEventsDetailsItem[] | null;
+    searchHit?: SearchHit;
+    id: string;
+  }) => {
+    const { browserFields } = useSourcererDataView(SourcererScopeName.detections);
+    const { from, to, shouldUpdate, selectedPatterns } = useTimelineDataFilters(false);
+    const [visiblePage, setVisiblePage] = useState('event');
+    const toggleButtons = [
+      {
+        id: 'event',
+        label: 'Event data',
+      },
+      {
+        id: 'sessionView',
+        label: 'Session view',
+      },
+      {
+        id: 'analyzer',
+        label: 'Analyzer graph',
+      },
+      {
+        id: 'json',
+        label: 'JSON',
+      },
+    ];
+    const onChange = (optionId: string) => {
+      setVisiblePage(optionId);
+    };
+
+    const { Navigation } = useSessionViewNavigation({
+      scopeId: TableId.alertsOnAlertsPage,
+    });
+    const { SessionView, shouldShowDetailsPanel, DetailsPanel } = useSessionView({
+      scopeId: TableId.alertsOnAlertsPage,
+    });
+
+    return (
+      <>
+        {data && (
+          <>
+            <EuiSpacer size="m" />
+            <EuiButtonGroup
+              color="primary"
+              idSelected={visiblePage}
+              legend="Visualization pages"
+              onChange={onChange}
+              options={toggleButtons}
+            />
+            <EuiSpacer size="xl" />
+            <EuiFlexGroup direction="column">
+              {visiblePage === 'event' && (
+                <>
+                  <EuiSpacer size="m" />
+                  <EuiTitle size="m">
+                    <h2>{'All fields'}</h2>
+                  </EuiTitle>
+                  <EuiSpacer size="m" />
+                  <EventFieldsBrowser
+                    browserFields={browserFields}
+                    data={data}
+                    eventId={id}
+                    isDraggable={false}
+                    scopeId={TableId.alertsOnAlertsPage}
+                    timelineTabType="flyout"
+                    isReadOnly={false}
+                  />
+                </>
+              )}
+              {visiblePage === 'sessionView' && (
+                <>
+                  <EuiSpacer size="m" />
+                  <SessionViewWrapper>{SessionView}</SessionViewWrapper>
+                </>
+              )}
+              {visiblePage === 'json' && (
+                <>
+                  <EuiSpacer size="m" />
+                  <JsonView rawEventData={searchHit} />
+                </>
+              )}
+              {visiblePage === 'analyzer' && (
+                <>
+                  <EuiSpacer size="m" />
+                  <Resolver
+                    databaseDocumentID={id}
+                    resolverComponentInstanceID={TableId.alertsOnAlertsPage}
+                    indices={selectedPatterns}
+                    shouldUpdate={shouldUpdate}
+                    filters={{ from, to }}
+                  />
+                </>
+              )}
+            </EuiFlexGroup>
+          </>
+        )}
+      </>
+    );
+  }
+);
+
+AlertDetailsVisualizeTab.displayName = 'AlertDetailsVisualizeTab';
