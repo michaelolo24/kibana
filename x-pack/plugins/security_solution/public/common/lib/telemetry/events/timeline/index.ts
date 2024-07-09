c59c1eb1c0b3b1dbc8a3b339381d1060a51bb759
Michael Olorunnisola <michael.olorunnisola@elastic.co>
2024-07-09 10:44:39 -0400

    start process

---
+++ x-pack/plugins/security_solution/public/common/lib/telemetry/events/timeline/index.ts
@@ -0,0 +1,84 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { TelemetryEvent } from '../../types';
+import { TelemetryEventTypes } from '../../constants';
+/**
+Opening timeline (Investigate in timeline action)
+Enabling/Disabling the query builder
+Enabling/Disabling row rendering
+Eql query usage
+Tabs usage
+Pinning event action
+Notes action
+Saving Timeline
+ */
+
+export const openTimeline: TelemetryEvent = {
+  eventType: TelemetryEventTypes.OpenNoteInExpandableFlyoutClicked,
+  schema: {
+    location: {
+      type: 'text',
+      _meta: {
+        description: 'Table ID or timeline ID',
+        optional: false,
+      },
+    },
+  },
+};
+
+export const pinnedTimelineEvent: TelemetryEvent = {
+  eventType: TelemetryEventTypes.AddNoteFromExpandableFlyoutClicked,
+  schema: {
+    isRelatedToATimeline: {
+      type: 'boolean',
+      _meta: {
+        description: 'If the note was added related to a saved timeline',
+        optional: false,
+      },
+    },
+  },
+};
+
+export const addedTimelineNote: TelemetryEvent = {
+  eventType: TelemetryEventTypes.AddNoteFromExpandableFlyoutClicked,
+  schema: {
+    isRelatedToATimeline: {
+      type: 'boolean',
+      _meta: {
+        description: 'If the note was added related to a saved timeline',
+        optional: false,
+      },
+    },
+  },
+};
+
+export const toggledTimelineQueryBuilder: TelemetryEvent = {
+  eventType: TelemetryEventTypes.AddNoteFromExpandableFlyoutClicked,
+  schema: {
+    isRelatedToATimeline: {
+      type: 'boolean',
+      _meta: {
+        description: 'If the note was added related to a saved timeline',
+        optional: false,
+      },
+    },
+  },
+};
+
+export const savedTimeline: TelemetryEvent = {
+  eventType: TelemetryEventTypes.AddNoteFromExpandableFlyoutClicked,
+  schema: {
+    isRelatedToATimeline: {
+      type: 'boolean',
+      _meta: {
+        description: 'If the note was added related to a saved timeline',
+        optional: false,
+      },
+    },
+  },
+};
